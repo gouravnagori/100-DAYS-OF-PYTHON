47f3acc92dee9e162c14b4adbc93965279c69223
gouravnagori <gouravnagori189@gmail.com>
2026-06-07 18:16:47 +0530

    Day 59 update

--- DAY_59/01_decorators.py
+++ DAY_59/01_decorators.py
@@ -26,13 +26,65 @@
 # arguments and return value of a function each time it is called:
 
 
-def changecase(func):
-    def myinner():
-        return func().upper()
-    return myinner
+def greet(fx):
+    def mfx(*args,**kargs):
+        print("Hii How's your day")
+        fx(*args,**kargs)
+        print("End of the code")
+    return mfx
 
-@changecase
-def myfunction():
-    return "Hello World"
+@greet
+def hello():
+    print("Yaa hooooo")
 
-print(myfunction())
+hello()    
+
+print('\n')
+@greet
+def sum(a,d):
+    print(a+d)
+
+sum(1,5)    
+
+
+
+# One common use of decorators is to add logging to a function. For
+# example, you could use a decorator to log the arguments and return
+# value of a function each time it is called:
+
+# import logging
+
+# def log_function_call(func):
+#   def decorated(*args, ** kwargs):
+#       logging. info(f"Calling {func .__ name_} with
+# args={args}, kwargs={kwargs}")
+#   result = func(*args, ** kwargs)
+#   logging. info(f"{func .__ name_} returned
+# {result}")
+#       return result
+#   return decorated
+
+# @log_function_call
+# def my_function(a, b):
+#   return a + b
+
+# In this example, the log_function_call decorator takes a function as
+# an argument and returns a new function that logs the function call
+# before and after the original function is called.
+
+
+
+# Conclusion
+
+# Decorators are a powerful and flexible feature in Python that can be
+# used to add functionality to functions and methods without
+# modifying their source code. They are a great tool for separating
+# concerns, reducing code duplication, and making your code more
+# readable and maintainable.
+
+# In conclusion, python decorators are a way to extend the
+# functionality of functions and methods, by modifying its behavior
+# without modifying the source code. They are used for a variety of
+# purposes, such as logging, memoization, access control, and more.
+# They are a powerful tool that can be used to make your code more
+# readable, maintainable, and extendable.
