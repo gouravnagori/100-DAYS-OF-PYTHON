0995129b93b5ccd71512d56007c9057ea7e4a3a2
gouravnagori <gouravnagori189@gmail.com>
2026-06-17 00:02:13 +0530

    DAY 76 is complete

---
+++ DAY_76/01_exer_08.py
@@ -0,0 +1,38 @@
+# Write a program to
+# manipulate pdf files
+# using pyPDF. Your
+# programs should be
+# able to merge
+# multiple pdf files
+# into a single pdf.
+# You are welcome to
+# add more
+# functionalities
+
+# pypdf is a free
+# and open-source pure-
+# python PDF library
+# capable of
+# splitting, merging,
+# cropping, and
+# transforming the
+# pages of PDF files.
+# It can also add
+# custom data, viewing
+# options, and
+# passwords to PDF
+# files. pypdf can
+# retrieve text and
+# metadata from PDFs
+# as well.
+
+
+from pypdf import PdfWriter
+
+merger = PdfWriter()
+
+for pdf in ["DAY_76/Basics of Linux OS.pdf","DAY_76/Linux Users and Groups.pdf"]:
+    merger.append(pdf)
+
+merger.write("DAY_76/Final.pdf")
+merger.close()
