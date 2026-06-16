4917abeeea7d42602c50bf2d0fbf0149bfd27c00
gouravnagori <gouravnagori189@gmail.com>
2026-06-16 17:25:06 +0530

    75% completed

---
+++ DAY_75/01_soln_exe07.py
@@ -0,0 +1,16 @@
+# Write a program to clear the clutter inside a folder on your computer.
+# You should use os module to rename all the png images from 1.png all
+# the way till n.png where n is the number of png files in that folder. Do
+# the same for other file formats
+
+# import os
+# os.mkdir("DAY_68/data")
+
+import os
+i = 1
+files = os.listdir("DAY_75/data")
+for file in files:
+    if file.endswith(".png"):
+        print(file)
+        os.rename(f"DAY_75/data/{file}",f"DAY_75/data/{i}.png")    
+        i += 1
